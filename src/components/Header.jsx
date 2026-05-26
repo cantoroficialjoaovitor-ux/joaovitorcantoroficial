56bb9957fd538c57f6ba77ef7707358b16988a26
cantoroficialjoaovitor-ux <cantoroficialjoaovitor@gmail.com>
2026-05-26 18:18:28 -0300

    Update Cabeçalho.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,37 @@
-export default function Header() {
+export default function Cabecalho() {
   return (
     <header className="header">
-      <div className="container nav">
-        <h1 className="logo">João Vitor</h1>
 
-        <nav>
+      <div className="nav">
+
+        <div className="logo">
+          João Vitor
+        </div>
+
+        <div className="menu">
+
           <a href="#sobre">Sobre</a>
-          <a href="#social">Redes</a>
+
+          <a href="#agenda">Agenda</a>
+
           <a href="#contato">Contato</a>
-        </nav>
+
+          <a href="#redes">
+            Redes Sociais
+          </a>
+
+          <a
+            href="https://wa.me/5511998923563"
+            target="_blank"
+            className="menu-button"
+          >
+            Contratar Show
+          </a>
+
+        </div>
+
       </div>
+
     </header>
   );
 }
