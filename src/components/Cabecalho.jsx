390b1046ea565e46b1f20ce4dcfcca580abfca2f
cantoroficialjoaovitor-ux <cantoroficialjoaovitor@gmail.com>
2026-05-26 19:25:18 -0300

    Update Cabecalho.jsx

--- src/components/Cabecalho.jsx
+++ src/components/Cabecalho.jsx
@@ -1,64 +1,76 @@
+import logoInstagram from "../imagens/LOGO_INSTA-removebg-preview-Photoroom.png";
+import logoSpotify from "../imagens/logo-spotify-512-removebg-preview.png";
+import logoYoutube from "../imagens/Logo Youtube-Photoroom.png";
+
 export default function Cabecalho() {
   return (
-    <header
-      style={{
-        position: "fixed",
-        top: 0,
-        width: "100%",
-        background: "#000",
-        zIndex: 999,
-        borderBottom: "1px solid #222",
-      }}
-    >
-      <div
-        style={{
-          maxWidth: "1400px",
-          margin: "0 auto",
-          padding: "18px 40px",
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center",
-        }}
-      >
-        <h1
-          style={{
-            color: "#fff",
-            fontSize: "28px",
-            fontWeight: "700",
-            margin: 0,
-          }}
-        >
-          João Vitor
-        </h1>
+    <header className="cabecalho">
+      <div className="container-header">
+        <div className="logo">João Vitor</div>
 
-        <nav
-          style={{
-            display: "flex",
-            alignItems: "center",
-            gap: "35px",
-          }}
-        >
-          <a href="#sobre" style={link}>
-            Sobre
-          </a>
+        <nav className="menu">
+          <a href="#sobre">Sobre</a>
 
-          <a href="#agenda" style={link}>
-            Agenda
-          </a>
+          <a href="#agenda">Agenda</a>
 
-          <a href="#contato" style={link}>
-            Contato
-          </a>
+          <div className="dropdown">
+            <button className="dropbtn">
+              Redes Sociais
+            </button>
 
-          <a href="#redes" style={linkGold}>
-            Redes Sociais
-          </a>
+            <div className="dropdown-content">
+              <a
+                href="https://www.instagram.com/joaovitorcantoroficial"
+                target="_blank"
+                rel="noreferrer"
+                className="rede-link"
+              >
+                <img
+                  src={logoInstagram}
+                  alt="Instagram"
+                  className="icone-rede"
+                />
+
+                Instagram
+              </a>
+
+              <a
+                href="https://open.spotify.com/intl-pt/artist/1NcHjMiIbcuCDUW9sg0Fls?si=mrISh3mHTd2RUhMKfRphuQ"
+                target="_blank"
+                rel="noreferrer"
+                className="rede-link"
+              >
+                <img
+                  src={logoSpotify}
+                  alt="Spotify"
+                  className="icone-rede"
+                />
+
+                Spotify
+              </a>
+
+              <a
+                href="https://www.youtube.com/joaovitorcantoroficial"
+                target="_blank"
+                rel="noreferrer"
+                className="rede-link"
+              >
+                <img
+                  src={logoYoutube}
+                  alt="YouTube"
+                  className="icone-rede"
+                />
+
+                YouTube
+              </a>
+            </div>
+          </div>
 
           <a
             href="https://wa.me/5511986894866"
             target="_blank"
             rel="noreferrer"
-            style={botao}
+            className="botao-show"
           >
             Contratar Show
           </a>
@@ -67,26 +79,3 @@ export default function Cabecalho() {
     </header>
   );
 }
-
-const link = {
-  color: "#fff",
-  textDecoration: "none",
-  fontWeight: "500",
-  fontSize: "18px",
-};
-
-const linkGold = {
-  color: "#d4af37",
-  textDecoration: "none",
-  fontWeight: "600",
-  fontSize: "18px",
-};
-
-const botao = {
-  background: "#d4af37",
-  color: "#000",
-  padding: "14px 24px",
-  borderRadius: "12px",
-  textDecoration: "none",
-  fontWeight: "700",
-};
