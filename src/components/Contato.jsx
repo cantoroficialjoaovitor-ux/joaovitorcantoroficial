b7b64c394e9aabdf6d4a60d948a8dd00bced33fc
cantoroficialjoaovitor-ux <cantoroficialjoaovitor@gmail.com>
2026-05-26 17:35:48 -0300

    Update Contato.jsx

--- src/components/Contato.jsx
+++ src/components/Contato.jsx
@@ -1,47 +1,21 @@
 export default function Contato() {
   return (
-    <section
-      id="contato"
-      style={{
-        background: "#111",
-        color: "#fff",
-        padding: "100px 20px",
-        textAlign: "center",
-      }}
-    >
-      <h2
-        style={{
-          fontSize: "42px",
-          marginBottom: "30px",
-        }}
-      >
-        Contato
-      </h2>
+    <section className="contato" id="contato">
+      <div className="container">
+        <h2>Contratações</h2>
 
-      <p
-        style={{
-          fontSize: "22px",
-          marginBottom: "30px",
-        }}
-      >
-        Contrate João Vitor para seu evento
-      </p>
+        <p>
+          Faça seu evento se tornar inesquecível.
+        </p>
 
-      <a
-        href="https://wa.me/5511986894866"
-        target="_blank"
-        style={{
-          background: "#ffd000",
-          color: "#000",
-          padding: "18px 40px",
-          borderRadius: "12px",
-          fontWeight: "bold",
-          textDecoration: "none",
-          fontSize: "20px",
-        }}
-      >
-        Chamar no WhatsApp
-      </a>
+        <a
+          href="https://wa.me/5511986894866"
+          target="_blank"
+          className="hero-btn"
+        >
+          Chamar no WhatsApp
+        </a>
+      </div>
     </section>
-  )
+  );
 }
