40be9f1e9a66d2f67f5036f7b2f1d47bf6825af1
cantoroficialjoaovitor-ux <cantoroficialjoaovitor@gmail.com>
2026-05-26 17:33:33 -0300

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,27 +1,30 @@
-import foto1 from "../images/foto1.png"
+import foto1 from "../images/foto1.png";
 
 export default function Hero() {
   return (
-    <section className="hero">
-      <img src={foto1} alt="João Vitor" />
-
-      <div className="overlay"></div>
-
+    <section
+      className="hero"
+      style={{
+        backgroundImage: `linear-gradient(rgba(0,0,0,0.65), rgba(0,0,0,0.85)), url(${foto1})`,
+      }}
+    >
       <div className="hero-content">
+        <span className="mini-title">Cantor Sertanejo</span>
+
         <h1>João Vitor</h1>
 
         <p>
-          Shows • Agenda • Contratações
+          Shows • Eventos • Casamentos • Barzinhos • Grandes Eventos
         </p>
 
         <a
-          className="btn"
           href="https://wa.me/5511986894866"
           target="_blank"
+          className="hero-btn"
         >
           Contratar Show
         </a>
       </div>
     </section>
-  )
+  );
 }
