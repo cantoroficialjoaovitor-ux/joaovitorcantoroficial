cfac144bcb5b9464835620197e765bd92021a38f
cantoroficialjoaovitor-ux <cantoroficialjoaovitor@gmail.com>
2026-05-26 17:33:05 -0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,63 +1,15 @@
 export default function Header() {
   return (
-    <header
-      style={{
-        background: "#000",
-        padding: "20px",
-        position: "fixed",
-        width: "100%",
-        top: 0,
-        zIndex: 999,
-      }}
-    >
-      <div
-        style={{
-          maxWidth: "1200px",
-          margin: "0 auto",
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center",
-        }}
-      >
-        <h1
-          style={{
-            color: "#fff",
-            fontSize: "28px",
-            fontWeight: "bold",
-          }}
-        >
-          João Vitor
-        </h1>
+    <header className="header">
+      <div className="container nav">
+        <h1 className="logo">João Vitor</h1>
 
-        <nav
-          style={{
-            display: "flex",
-            gap: "20px",
-          }}
-        >
-          <a href="#sobre" style={link}>
-            Sobre
-          </a>
-
-          <a href="#galeria" style={link}>
-            Galeria
-          </a>
-
-          <a href="#contato" style={link}>
-            Contato
-          </a>
-
-          <a href="#agenda" style={link}>
-            Agenda
-          </a>
+        <nav>
+          <a href="#sobre">Sobre</a>
+          <a href="#social">Redes</a>
+          <a href="#contato">Contato</a>
         </nav>
       </div>
     </header>
-  )
-}
-
-const link = {
-  color: "#fff",
-  textDecoration: "none",
-  fontWeight: "bold",
+  );
 }
