95eb7479273f45983a787b221329ff3e8c2f54f0
cantoroficialjoaovitor-ux <cantoroficialjoaovitor@gmail.com>
2026-05-26 18:05:30 -0300

    Create Redes.jsx

---
+++ src/components/Redes.jsx
@@ -0,0 +1,59 @@
+export default function Redes() {
+  return (
+    <section className="social">
+
+      <span className="social-mini">
+        ACOMPANHE
+      </span>
+
+      <h2>
+        NAS REDES SOCIAIS
+      </h2>
+
+      <div className="social-grid">
+
+        <a
+          href="https://instagram.com/joaovitorcantoroficial"
+          target="_blank"
+          className="social-card"
+        >
+          <div className="icon instagram">
+            📸
+          </div>
+
+          <h3>INSTAGRAM</h3>
+
+          <p>@joaovitorcantoroficial</p>
+        </a>
+
+        <a
+          href="#"
+          className="social-card"
+        >
+          <div className="icon spotify">
+            🎵
+          </div>
+
+          <h3>SPOTIFY</h3>
+
+          <p>Ouça agora</p>
+        </a>
+
+        <a
+          href="https://www.youtube.com/joaovitorcantoroficial"
+          target="_blank"
+          className="social-card"
+        >
+          <div className="icon youtube">
+            ▶️
+          </div>
+
+          <h3>YOUTUBE</h3>
+
+          <p>youtube.com/joaovitorcantoroficial</p>
+        </a>
+
+      </div>
+    </section>
+  );
+}
