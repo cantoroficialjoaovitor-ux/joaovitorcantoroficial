fd8ccef06d93d97a2da406eeab61277ba66f6537
cantoroficialjoaovitor-ux <cantoroficialjoaovitor@gmail.com>
2026-05-26 19:26:38 -0300

    Update Sobre.jsx

--- src/components/Sobre.jsx
+++ src/components/Sobre.jsx
@@ -1,13 +1,60 @@
 export default function Sobre() {
   return (
-    <section className="sobre" id="sobre">
+    <section id="sobre" className="sobre">
       <div className="container">
-        <h2>Sobre</h2>
+        <h2>Sobre João Vitor</h2>
 
         <p>
-          João Vitor é cantor sertanejo apaixonado pela música,
-          levando emoção, presença de palco e animação para eventos,
-          bares, casamentos e grandes shows.
+          João Vitor é um talentoso artista nascido na charmosa cidade de
+          Lambari, localizada no sul de Minas Gerais.
+        </p>
+
+        <p>
+          Sua abordagem musical se destaca de forma distinta em relação aos
+          artistas sertanejos que dominam o cenário atual.
+        </p>
+
+        <p>
+          O que o diferencia é o seu timbre de voz singular e a sua notável
+          criatividade, elementos que têm impulsionado sua ascensão no universo
+          da música sertaneja no Brasil.
+        </p>
+
+        <p>
+          Ao longo de sua carreira, João Vitor passou por diversos projetos
+          musicais em parceria com outros artistas, mas hoje ele se reinventa e
+          trilha seu próprio caminho como cantor solo.
+        </p>
+
+        <p>
+          Seus EPs, amplamente disponíveis em todas as plataformas de música,
+          são uma jornada nostálgica pelos antigos sucessos que marcaram uma era
+          no sertanejo.
+        </p>
+
+        <p>
+          Em 2022, João Vitor presenteou seu público com seu primeiro EP solo,
+          intitulado "João Vitor Acústico".
+        </p>
+
+        <p>
+          Em 2023, o cantor lançou dois novos EPs, "João Vitor Resenha" e
+          "João Vitor Acústico 2".
+        </p>
+
+        <p>
+          Além disso, João Vitor lançou sua primeira canção inédita e original,
+          "Desejo".
+        </p>
+
+        <p>
+          Com uma banda robusta, talentosa e uma equipe técnica completa, o
+          show de João Vitor é sinônimo de garantia de qualidade e carisma.
+        </p>
+
+        <p>
+          Sua trajetória promissora continua a surpreender e encantar amantes da
+          música sertaneja em todo o país.
         </p>
       </div>
     </section>
