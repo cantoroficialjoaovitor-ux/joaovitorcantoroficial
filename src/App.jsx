d50bf527dd50fa028742a382b306f2a2d5a792f2
cantoroficialjoaovitor-ux <cantoroficialjoaovitor@gmail.com>
2026-06-18 20:47:57 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
-import "./index.css";
-
-import Cabecalho from "./componentes/Cabecalho";
-import Hero from "./componentes/Hero";
-import Sobre from "./componentes/Sobre";
-import LiveMarketing from "./componentes/LiveMarketing";
-import Manifesto from "./componentes/Manifesto";
-import Experiencias from "./componentes/Experiencias";
-import Agenda from "./componentes/Agenda";
-import Videos from "./componentes/Videos";
-import Galeria from "./componentes/Galeria";
-import Redes from "./componentes/Redes";
-import Contato from "./componentes/Contato";
-import Contratacao from "./componentes/Contratacao";
-import Footer from "./componentes/Footer";
+import Cabecalho from "./components/Cabecalho";
+import Hero from "./components/Hero";
+import Sobre from "./components/Sobre";
+import LiveMarketing from "./components/LiveMarketing";
+import Manifesto from "./components/Manifesto";
+import Experiencias from "./components/Experiencias";
+import Agenda from "./components/Agenda";
+import Videos from "./components/Videos";
+import Galeria from "./components/Galeria";
+import Redes from "./components/Redes";
+import Contato from "./components/Contato";
+import Contratacao from "./components/Contratacao";
+import Footer from "./components/Footer";
 
 function App() {
   return (
