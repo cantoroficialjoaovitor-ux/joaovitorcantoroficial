48c86812ad90294fa86f113636c3004dad9fba29
cantoroficialjoaovitor-ux <cantoroficialjoaovitor@gmail.com>
2026-06-18 19:02:29 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,38 @@
 import "./index.css";
 
-import Cabecalho from "./components/Cabecalho";
-import Hero from "./components/Hero";
-import Sobre from "./components/Sobre";
+import Cabecalho from "./componentes/Cabecalho";
+import Hero from "./componentes/Hero";
+import Sobre from "./componentes/Sobre";
+import Agenda from "./componentes/Agenda";
+import Videos from "./componentes/Videos";
+import Galeria from "./componentes/Galeria";
+import Redes from "./componentes/Redes";
+import Contato from "./componentes/Contato";
+import Contratacao from "./componentes/Contratacao";
+import Footer from "./componentes/Footer";
 
 function App() {
   return (
     <>
       <Cabecalho />
+
       <Hero />
+
       <Sobre />
+
+      <Agenda />
+
+      <Videos />
+
+      <Galeria />
+
+      <Redes />
+
+      <Contato />
+
+      <Contratacao />
+
+      <Footer />
     </>
   );
 }
